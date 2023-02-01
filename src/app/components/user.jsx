89ca75f3e47980666b84f0bfa07f36bbc6797a18
Sergey Ezdakov <esdakov@mail.ru>
2023-02-02 01:50:48 +0300

    answer the lesson1 4.Pagination

--- src/app/components/user.jsx
+++ src/app/components/user.jsx
@@ -1,47 +1,64 @@
-import React from 'react'
-import Qualitie from './qualitie'
-import BookMark from './bookmark'
+import React from "react";
+import Qualitie from "./qualitie";
+import BookMark from "./bookmark";
+import PropTypes from "prop-types";
 
 const User = ({
-   _id, 
-   name, 
-   qualities, 
-   profession, 
-   completedMeetings, 
-   rate, 
-   handleDelete, 
-   bookmark, 
-   onToggleBookMark
+    _id,
+    name,
+    qualities,
+    profession,
+    completedMeetings,
+    rate,
+    handleDelete,
+    bookmark,
+    onToggleBookMark
 }) => {
-   return (
-      <>
-         <tr key={_id}>
-            <td>{name}</td>
-            <td>
-               {qualities.map((qualitie) => (
-                  <Qualitie color={qualitie.color} key={qualitie._id} name={qualitie.name} />
-               ))}
-            </td>
-            <td>{profession.name}</td>
-            <td>{completedMeetings}</td>
-            <td>{rate} /5</td>
-            <td>
-               <BookMark
-                  status={bookmark}
-                  onClick={()=>onToggleBookMark(_id)}
-               />
-            </td>
-            <td>
-               <button
-                  className={"btn btn-danger"}
-                  onClick={() => handleDelete(_id)}
-               >
-                  Удалить
-               </button>
-            </td>
-         </tr>
-      </>
-   )
-}
+    return (
+        <>
+            <tr key={_id}>
+                <td>{name}</td>
+                <td>
+                    {qualities.map((qualitie) => (
+                        <Qualitie
+                            color={qualitie.color}
+                            key={qualitie._id}
+                            name={qualitie.name}
+                        />
+                    ))}
+                </td>
+                <td>{profession.name}</td>
+                <td>{completedMeetings}</td>
+                <td>{rate} /5</td>
+                <td>
+                    <BookMark
+                        status={bookmark}
+                        onClick={() => onToggleBookMark(_id)}
+                    />
+                </td>
+                <td>
+                    <button
+                        className={"btn btn-danger"}
+                        onClick={() => handleDelete(_id)}
+                    >
+                        Удалить
+                    </button>
+                </td>
+            </tr>
+        </>
+    );
+};
 
-export default User
+User.propTypes = {
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    qualities: PropTypes.array.isRequired,
+    profession: PropTypes.object.isRequired,
+    completedMeetings: PropTypes.number.isRequired,
+    rate: PropTypes.number.isRequired,
+    handleDelete: PropTypes.func.isRequired,
+    bookmark: PropTypes.bool.isRequired,
+    onToggleBookMark: PropTypes.func.isRequired
+};
+
+export default User;
