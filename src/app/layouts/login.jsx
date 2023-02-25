d5c7e769e8c19b688e9ccd554e6d559c7397c6e2
Sergey Ezdakov <esdakov@mail.ru>
2023-02-26 00:05:48 +0300

    lesson 7 11. password validate

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -3,61 +3,71 @@ import TextField from "../components/testField";
 import { validator } from "../utils/validator";
 
 const Login = () => {
-    const [data, setData] = useState({ email: "", password: "" });
-    const [errors, setErrors] = useState({});
-    const handleChange = ({ target }) => {
-        setData((prevState) => ({
-            ...prevState,
-            [target.name]: target.value
-        }));
-    };
-    const validatorConfig = {
-        email: {
-            isRequired: {
-                message: "Электронная почта обязательно для заполнения"
-            },
-            isEmail: {
-                message: "Email введен некорректно"
-            }
-        },
-        password: {
-            isRequired: { message: "Пароль обязателен для заполнения" }
-        }
-    };
-    useEffect(() => {
-        validate();
-    }, [data]);
-    const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const isValid = validate();
-        if (!isValid) return;
-        console.log(data);
-    };
-    return (
-        <form onSubmit={handleSubmit}>
-            <TextField
-                label="Электронная почта"
-                name="email"
-                value={data.email}
-                onChange={handleChange}
-                error={errors.email}
-            />
-            <TextField
-                label="Пароль"
-                type="password"
-                name="password"
-                value={data.password}
-                onChange={handleChange}
-                error={errors.password}
-            />
-            <button type="submit">Отправить</button>
-        </form>
-    );
+   const [data, setData] = useState({ email: "", password: "" });
+   const [errors, setErrors] = useState({});
+   const handleChange = ({ target }) => {
+      setData((prevState) => ({
+         ...prevState,
+         [target.name]: target.value
+      }));
+   };
+   const validatorConfig = {
+      email: {
+         isRequired: {
+            message: "Электронная почта обязательно для заполнения"
+         },
+         isEmail: {
+            message: "Email введен некорректно"
+         }
+      },
+      password: {
+         isRequired: { message: "Пароль обязателен для заполнения" },
+         isCapitalSymbol: {
+            message: "Пароль должен содержать хотя бы одну заглавную букву"
+         },
+         isContainDigit: {
+            message: "Пароль должен содержать хотя бы одно число"
+         },
+         min: {
+            message: "Пароль должен состоять минимум из 8 символов",
+            value: 8
+         }
+      }
+   };
+   useEffect(() => {
+      validate();
+   }, [data]);
+   const validate = () => {
+      const errors = validator(data, validatorConfig);
+      setErrors(errors);
+      return Object.keys(errors).length === 0;
+   };
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      const isValid = validate();
+      if (!isValid) return;
+      console.log(data);
+   };
+   return (
+      <form onSubmit={handleSubmit}>
+         <TextField
+            label="Электронная почта"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            error={errors.email}
+         />
+         <TextField
+            label="Пароль"
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            error={errors.password}
+         />
+         <button type="submit">Отправить</button>
+      </form>
+   );
 };
 
 export default Login;
