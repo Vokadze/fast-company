f71fefb4ef0b00c4d625e965a4b41d2fb1ed9d4f
Sergey Ezdakov <esdakov@mail.ru>
2023-04-16 17:14:54 +0300

    chenge error in selectField.jsx

--- src/app/components/common/form/selectField.jsx
+++ src/app/components/common/form/selectField.jsx
@@ -55,7 +55,7 @@ SelectField.propTypes = {
     onChange: PropTypes.func,
     defaultOption: PropTypes.string,
     options: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-        name: PropTypes.oneOfType([
+    name: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.object,
         PropTypes.array
