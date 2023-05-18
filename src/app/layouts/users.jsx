c31b4411401831af9d8c0548e3e6e092c5d5a19a
Sergey Ezdakov <esdakov@mail.ru>
2023-05-18 22:52:21 +0300

    lesson 17 chenge users.jsx

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -4,22 +4,22 @@ import { useParams, Redirect } from "react-router-dom";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/userListPage";
 import EditUserPage from "../components/page/editUserPage/editUserPage";
-import { useAuth } from "../hooks/useAuth";
+// import { useAuth } from "../hooks/useAuth";
 import UserProvider from "../hooks/useUsers";
 
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
-    const { currentUser } = useAuth();
+    // const { currentUser } = useAuth();
     return (
         <>
             <UserProvider>
                 {userId ? (
                     edit ? (
-                        userId === currentUser._id ? (
+                        userId ? (
                             <EditUserPage />
                         ) : (
-                            <Redirect to={`/users/${currentUser._id}/edit`} />
+                            <Redirect to={`/users/:userId/:edit`} />
                         )
                     ) : (
                         <UserPage userId={userId} />
