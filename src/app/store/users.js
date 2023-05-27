e2fe4c7d9e39fea1b3d24b46c3b2feaa111f5bff
Sergey Ezdakov <esdakov@mail.ru>
2023-05-28 00:52:16 +0300

    exercise 2 in Implementing Redux in a project. Fast Company

--- src/app/store/users.js
+++ src/app/store/users.js
@@ -57,6 +57,11 @@ const usersSlice = createSlice({
             state.isLoggedIn = false;
             state.auth = null;
             state.dataLoaded = false;
+        },
+        userUpdateSuccess: (state, action) => {
+            state.entities[
+                state.entities.findIndex((u) => u._id === action.payload._id)
+            ] = action.payload;
         }
     }
 });
@@ -69,12 +74,15 @@ const {
     authRequestSuccess,
     authRequestFailed,
     userCreated,
-    userLoggedOut
+    userLoggedOut,
+    userUpdateSuccess
 } = actions;
 
 const authRequested = createAction("users/authRequested");
 const userCreateRequested = createAction("users/userCreateRequested");
 const createUserFailed = createAction("users/createUserFailed");
+const userUpdateRequested = createAction("users/userUpdateRequested");
+const userUpdateFailed = createAction("users/userUpdateFailed");
 
 export const login =
     ({ payload, redirect }) =>
@@ -144,6 +152,17 @@ export const loadUsersList = () => async (dispatch, getState) => {
         dispatch(usersReceved(content));
     } catch (error) {
         dispatch(usersRequestFiled(error.message));
+    }
+};
+
+export const updateUser = (payload) => async (dispatch) => {
+    dispatch(userUpdateRequested());
+    try {
+        const { content } = await userService.update(payload);
+        dispatch(userUpdateSuccess(content));
+        history.push("/users/" + `${content._id}`);
+    } catch (error) {
+        dispatch(userUpdateFailed(error.message));
     }
 };
 
